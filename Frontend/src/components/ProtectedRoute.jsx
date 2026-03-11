7f388b75422b0d9e38b56e76df1eb88987f25466
piyush186sharma <piyushsharmaspsghy@gmail.com>
2026-03-11 19:45:45 +0530

    frontend fix 2

--- Frontend/src/components/ProtectedRoute.jsx
+++ Frontend/src/components/ProtectedRoute.jsx
@@ -4,7 +4,7 @@ const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("accessToken");
 
   if (!token) {
-    return <Navigate to="/signin" replace />;
+    return <Navigate to="/auth" replace />;
   }
 
   return children;
