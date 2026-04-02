6a32ca08238b849f5bcc253bd7ca22662aa9d645
piyush186sharma <piyushsharmaspsghy@gmail.com>
2026-04-02 12:54:40 +0530

    ml model live

--- Frontend/src/pages/cropRecomendation.tsx
+++ Frontend/src/pages/cropRecomendation.tsx
@@ -213,7 +213,7 @@ export default function CropRecommendation() {
   setLoading(true);
 
   try {
-    const response = await fetch("http://127.0.0.1:5001/predict", {
+    const response = await fetch("https://irrigo3-0-1.onrender.com/predict", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
